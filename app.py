0ae160b5bbec0472de88bff749958a56f38cdfdd
lakshya-bandi25 <22251a1202@gmail.com>
2025-10-23 21:25:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,4 +26,4 @@ def result():
     return render_template('result.html', username=username)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)  # Port changed to 5000
+    app.run(host="0.0.0.0", port=5000, debug=True)  # Port changed to 5000
